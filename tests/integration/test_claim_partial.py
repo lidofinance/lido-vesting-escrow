58423e58982cff130611b11afba761f4b615554d
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-11-24 19:56:25 +0400

    fix: integration test

--- tests/integration/test_claim_partial.py
+++ tests/integration/test_claim_partial.py
@@ -3,7 +3,7 @@ from brownie.test import given, strategy
 
 @given(sleep_time=strategy("uint", max_value=100000))
 def test_claim_partial(
-    activated_vesting,
+    deployed_vesting,
     token,
     recipient,
     chain,
@@ -13,7 +13,7 @@ def test_claim_partial(
     balance,
 ):
     chain.sleep(start_time - chain.time() + sleep_time)
-    tx = activated_vesting.claim({"from": recipient})
+    tx = deployed_vesting.claim({"from": recipient})
     expected_amount = balance * (tx.timestamp - start_time) // (end_time - start_time)
 
     assert token.balanceOf(recipient) == expected_amount

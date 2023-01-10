de9a5f05b7cb5be1c0c202cee1a64e16f1e94c1b
skhomuti <skhomuti@gmail.com>
2023-01-10 18:09:32 +0500

    Add more recover tests

--- tests/functional/VestingEscrow/test_revoke_all.py
+++ tests/functional/VestingEscrow/test_revoke_all.py
@@ -1,7 +1,8 @@
 import brownie
-import pytest
 
-pytestmark = [pytest.mark.parametrize("deployed_vesting", [pytest.param(1, id="fully_revocable")], indirect=True)]
+from tests.conftest import fully_revocable
+
+pytestmark = [fully_revocable]
 
 
 def test_revoke_all_owner_only(deployed_vesting, not_owner):
@@ -36,10 +37,10 @@ def test_revoke_all_before_start_time(deployed_vesting, token, owner, recipient,
     assert token.balanceOf(owner) == balance
 
 
-def test_revoke_all_partially_unclaimed(
-    deployed_vesting, token, owner, recipient, chain, start_time, end_time, balance
+def test_claim_revoked(
+    deployed_vesting, token, owner, recipient, chain, start_time, end_time, sleep_time, balance
 ):
-    chain.sleep(start_time - chain.time() + 31337)
+    chain.sleep(start_time - chain.time() + sleep_time)
     deployed_vesting.revoke_all({"from": owner})
     chain.sleep(end_time - chain.time())
     deployed_vesting.claim({"from": recipient})
@@ -50,9 +51,8 @@ def test_revoke_all_partially_unclaimed(
 
 
 def test_revoke_all_partially_claimed(
-    deployed_vesting, token, owner, recipient, chain, start_time, end_time, balance
+    deployed_vesting, token, owner, recipient, chain, start_time, end_time, sleep_time, balance
 ):
-    sleep_time = (end_time - start_time) // 2
     chain.sleep(start_time - chain.time() + sleep_time)
     claim_amount = 10 ** 18
     deployed_vesting.claim(recipient, claim_amount, {"from": recipient})
@@ -62,16 +62,3 @@ def test_revoke_all_partially_claimed(
 
     assert token.balanceOf(recipient) == claim_amount
     assert token.balanceOf(owner) == balance - claim_amount
-
-
-def test_recover_extra_after_revoke_all(deployed_vesting, token, balance, recipient, owner, chain, start_time, end_time):
-    extra = 10**17
-    token._mint_for_testing(extra, {"from": owner})
-    token.transfer(deployed_vesting, extra, {"from": owner})
-
-    chain.sleep(start_time - chain.time() + 31337)
-    deployed_vesting.revoke_all({"from": owner})
-    assert token.balanceOf(owner) == balance
-
-    deployed_vesting.recover_erc20(token, balance, {"from": recipient})
-    assert token.balanceOf(recipient) == extra

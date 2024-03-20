b83b982b98b945ac5c964197cc6382437cacb1c5
Alexander Belokon <ikatamarinaki@gmail.com>
2024-03-20 11:31:19 +0100

    test: add resetDelegate test

--- tests/functional/VestingEscrow/test_delegate.py
+++ tests/functional/VestingEscrow/test_delegate.py
@@ -1,22 +1,30 @@
 import brownie
+from brownie import ZERO_ADDRESS
 
 
 def test_delegate(deployed_vesting, recipient, voting_adapter):
     data = voting_adapter.encode_delegate_calldata(recipient)
     tx = deployed_vesting.delegate(data, {"from": recipient})
     assert len(tx.events) == 1
-    assert tx.events[0]["voter"] == deployed_vesting.address
-    assert tx.events[0]["delegate"] == recipient
+    assert tx.events["SetDelegate"]["voter"] == deployed_vesting.address
+    assert tx.events["SetDelegate"]["delegate"] == recipient.address
+
+    reset_data = voting_adapter.encode_delegate_calldata(ZERO_ADDRESS)
+    tx_reset = deployed_vesting.delegate(reset_data, {"from": recipient})
+    assert len(tx_reset.events) == 1
+    assert tx_reset.events["ResetDelegate"]["voter"] == deployed_vesting.address
+    assert tx_reset.events["ResetDelegate"]["delegate"] == recipient.address
 
 
-def test_vote_after_upgrade(
+def test_delegate_after_upgrade(
     deployed_vesting, recipient, voting_adapter_for_update, vesting_factory, owner, voting_adapter
 ):
     vesting_factory.update_voting_adapter(voting_adapter_for_update, {"from": owner})
     data = voting_adapter.encode_delegate_calldata(recipient)
     tx = deployed_vesting.delegate(data, {"from": recipient})
     assert len(tx.events) == 1
-    assert tx.events[0]["voter"] == deployed_vesting.address
+    assert tx.events["SetDelegate"]["voter"] == deployed_vesting.address
+    assert tx.events["SetDelegate"]["delegate"] == recipient.address
 
 
 def test_delegate_from_not_recipient_fail(deployed_vesting, not_recipient, voting_adapter):
--- tests/functional/VotingAdapter/test_delegate.py
+++ tests/functional/VotingAdapter/test_delegate.py
@@ -1,6 +1,14 @@
-
+from brownie import ZERO_ADDRESS
 
 def test_delegate(voting_adapter, recipient):
     data = voting_adapter.encode_delegate_calldata(recipient)
     tx = voting_adapter.delegate(data, {"from": recipient})
     assert len(tx.events) == 1
+    assert tx.events["SetDelegate"]["voter"] == voting_adapter.address
+    assert tx.events["SetDelegate"]["delegate"] == recipient.address
+
+    reset_data = voting_adapter.encode_delegate_calldata(ZERO_ADDRESS)
+    tx_reset = voting_adapter.delegate(reset_data, {"from": recipient})
+    assert len(tx_reset.events) == 1
+    assert tx_reset.events["ResetDelegate"]["voter"] == voting_adapter.address
+    assert tx_reset.events["ResetDelegate"]["delegate"] == recipient.address

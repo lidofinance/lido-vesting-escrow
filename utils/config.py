0e18a8ecdd36efa0297747a99632d4ef3e83ca8a
Vitaly Galaichuk <vitaly.galaichuk@gmail.com>
2025-05-27 20:35:12 +0700

    chore: add Hoodi testnet configuration and deployments

--- utils/config.py
+++ utils/config.py
@@ -5,6 +5,9 @@ from brownie.utils import color
 if network.show_active() == "holesky":
     print(f'Using {color("cyan")}vesting_initial_params_holesky.py{color} addresses')
     from vesting_initial_params_holesky import *
+elif network.show_active() == "hoodi":
+    print(f'Using {color("blue")}vesting_initial_params_hoodi.py{color} addresses')
+    from vesting_initial_params_hoodi import *
 else:
     print(f'Using {color("magenta")}vesting_initial_params.py{color} addresses')
     from vesting_initial_params import *
